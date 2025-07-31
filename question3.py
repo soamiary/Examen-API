f09dab3c8cf7d6dd5732d1831de1ab18219da742
Soa <hei.soa.3@gmail.com>
2025-07-31 19:22:35 +1300

    q3

--- question3.py
+++ question3.py
@@ -4,18 +4,8 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
-# Ton handler personnalisé pour les erreurs 404
 @app.exception_handler(StarletteHTTPException)
-async def custom_404_handler(request: Request, exc: StarletteHTTPException):
+async def not_found(request: Request, exc: StarletteHTTPException):
     if exc.status_code == 404:
-        html_404 = """
-        <html>
-            <head><title>404 NOT FOUND</title></head>
-            <body>
-                <h1>404 NOT FOUND</h1>
-            </body>
-        </html>
-        """
-        return HTMLResponse(content=html_404, status_code=404)
-    # Pour les autres erreurs, renvoyer l'exception par défaut
-    return await app.default_exception_handler(request, exc)
+        return HTMLResponse("<h1>404 NOT FOUND</h1>", status_code=404)
+    raise exc  
